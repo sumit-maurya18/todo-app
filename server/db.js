51276f46b6a2c9ea1fa470610f63bdd51dfad03b
sumit-maurya18 <techaid185@gmail.com>
2025-09-30 10:12:32 +0530

    Injected the right DB credentials and final working version

--- server/db.js
+++ server/db.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
+
+const isLocal = process.env.DB_HOST === "localhost";
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  ssl: isLocal ? false : { rejectUnauthorized: false }, // ✅ needed for Neon
 });
 
 module.exports = pool;
